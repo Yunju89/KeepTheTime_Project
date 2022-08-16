281ac8326b0720a6891f3162c150d1afac50395f
joo <toyaann@naver.com>
2022-08-16 18:08:28 +0900

    Retrofit 클래스 객체 / APIList 인터페이스 결합  - BaseActivity 상속처리

--- app/src/main/java/com/example/keepthetime_project/BaseActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/BaseActivity.kt
@@ -3,15 +3,22 @@ package com.example.keepthetime_project
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.keepthetime_project.api.APIList
+import com.example.keepthetime_project.api.ServerAPI
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext : Context
 
+    lateinit var apiList: APIList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mContext = this
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
 
     }
 
