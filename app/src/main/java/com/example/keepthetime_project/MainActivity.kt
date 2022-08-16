bb9a33f6457071b70f3cf3797f60ed574a732bd9
joo <toyaann@naver.com>
2022-08-16 14:12:22 +0900

    Initial commit

---
+++ app/src/main/java/com/example/keepthetime_project/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.keepthetime_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.keepthetime_project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
