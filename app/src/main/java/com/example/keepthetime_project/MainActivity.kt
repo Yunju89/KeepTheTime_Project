5d5010ec7543faad59e3c5d64c8cad983c3edb8a
joo <toyaann@naver.com>
2022-08-16 14:21:30 +0900

    viewBinding 적용

--- app/src/main/java/com/example/keepthetime_project/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/MainActivity.kt
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.keepthetime_project.databinding.ActivityMainBinding
 
+private lateinit var binding : ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
     }
