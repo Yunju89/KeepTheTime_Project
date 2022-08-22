979d5afeccd68e46a3b1c23fb4f448d37cb69fed
joo <toyaann@naver.com>
2022-08-22 16:05:14 +0900

    자동로그인 기능 구현 (내정보 조회 성공시)

--- app/src/main/java/com/example/keepthetime_project/SplashActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/SplashActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.keepthetime_project.databinding.ActivitySplashBinding
+import com.example.keepthetime_project.datas.BasicResponse
+import com.example.keepthetime_project.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
 
@@ -24,13 +29,33 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
+        var isMyInfoLoaded = false
+
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if(response.isSuccessful){
+                    isMyInfoLoaded = true
+
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed({
 
-            val myIntent = Intent(mContext, SignInActivity::class.java)
+            val myIntent : Intent = if (isMyInfoLoaded){
+                Intent(mContext, MainActivity::class.java)
+            }else{
+                Intent(mContext, SignInActivity::class.java)
+            }
+
             startActivity(myIntent)
-            finish()
 
         }, 2500)
 
