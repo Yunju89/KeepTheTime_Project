555d9ca3ba72e2b48b63cf2b193cf7ed0fe2da12
yunju <yunju891018@gmail.com>
2022-08-23 15:57:17 +0900

    친구 목록에 실제 데이터 반영

--- app/src/main/java/com/example/keepthetime_project/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/example/keepthetime_project/adapters/MyFriendAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.example.keepthetime_project.R
 import com.example.keepthetime_project.datas.UserData
 
@@ -20,8 +23,18 @@ class MyFriendAdapter(
         if (tempRow == null) {
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
         }
-
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val nickName = row.findViewById<TextView>(R.id.txtNickname)
+        val email = row.findViewById<TextView>(R.id.txtEmail)
+
+        Glide.with(mContext).load(data.profile_img).into(imgProfile)
+        nickName.text = data.nick_name
+        email.text = data.email
+
 
         return row
 
