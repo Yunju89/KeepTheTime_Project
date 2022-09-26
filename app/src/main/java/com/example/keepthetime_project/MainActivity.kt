82dfda8d5c9b515a9a803e4ce71cc1b9743cdd03
yunju <yunju891018@gmail.com>
2022-09-26 16:38:47 +0900

    MainActivity 구조 변경 -> BottomNavigation + ViewPager2  적용

--- app/src/main/java/com/example/keepthetime_project/MainActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.keepthetime_project
 
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.example.keepthetime_project.adapters.MainViewPagerAdapter
 import com.example.keepthetime_project.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -8,7 +9,6 @@ import com.google.android.material.tabs.TabLayoutMediator
 class MainActivity : BaseActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private lateinit var mAdapter : MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +23,39 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setEvents() {
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.myAppointment -> binding.viewpager.currentItem = 0
+                R.id.myProfile -> binding.viewpager.currentItem = 1
+            }
+            return@setOnItemSelectedListener true
+        }
 
+//        이벤트 처리 함수 직접 오버라이딩 필요
+        binding.viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                binding.bottomNav.selectedItemId = when(position){
+                    0 -> R.id.myAppointment
+                    else -> R.id.myProfile
+                }
+            }
+        })
     }
 
     override fun setValues() {
 
-        mAdapter = MainViewPagerAdapter(this)
-        binding.viewpager.adapter = mAdapter
+        binding.viewpager.adapter = MainViewPagerAdapter(this)// Activity 객체 -> Context 대입 불가
 
-        TabLayoutMediator(binding.tabLayout, binding.viewpager){ tab, position ->
-            when(position){
-                0 -> tab.text = "약속목록"
-                1 -> tab.text = "내프로필"
-            }
-        }.attach()
+
+//        탭 레이아웃 / 뷰페이저 2
+//        TabLayoutMediator(binding.tabLayout, binding.viewpager){ tab, position ->
+//            when(position){
+//                0 -> tab.text = "약속목록"
+//                1 -> tab.text = "내프로필"
+//            }
+//        }.attach()
 
     }
 }
