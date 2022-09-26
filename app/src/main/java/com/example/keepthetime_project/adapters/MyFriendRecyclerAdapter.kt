98ced80016ac54412354f1b4f0467e462e9e914b
yunju <yunju891018@gmail.com>
2022-09-26 11:21:52 +0900

    내 친구 목록 recyclerView 변경

--- app/src/main/java/com/example/keepthetime_project/adapters/MyFriendRecyclerAdapter.kt
+++ app/src/main/java/com/example/keepthetime_project/adapters/MyFriendRecyclerAdapter.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.keepthetime_project.R
 import com.example.keepthetime_project.datas.UserData
 
@@ -15,7 +18,38 @@ class MyFriendRecyclerAdapter(
 
     inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view){
 
+        val imgProfile = view.findViewById<ImageView>(R.id.imgProfile)
+        val nickName = view.findViewById<TextView>(R.id.txtNickname)
+        val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
+        val email = view.findViewById<TextView>(R.id.txtEmail)
 
+        fun bind(data: UserData){
+
+            Glide.with(mContext).load(data.profile_img).into(imgProfile)
+            nickName.text = data.nick_name
+
+            when (data.provider){
+                "default" -> {
+                    email.text = data.email
+                    Glide.with(mContext).load(data.profile_img).into(imgSocialLoginLogo)
+                }
+                "kakao" -> {
+                    email.text = "카카오 로그인"
+                    Glide.with(mContext).load(R.drawable.kakao_logo).into(imgSocialLoginLogo)
+                }
+                "naver" -> {
+                    email.text = "네이버 로그인"
+                    Glide.with(mContext).load(R.drawable.naver_logo).into(imgSocialLoginLogo)
+                }
+                "facebook" -> {
+                    email.text = "페이스북 로그인"
+                    Glide.with(mContext).load(R.drawable.facebook_logo).into(imgSocialLoginLogo)
+                }
+                else -> {
+
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -24,6 +58,9 @@ class MyFriendRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = mList[position]
+        holder.bind(data)
+
     }
 
     override fun getItemCount() = mList.size
