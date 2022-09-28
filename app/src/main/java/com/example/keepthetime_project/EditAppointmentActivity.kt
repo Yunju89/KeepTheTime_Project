e5f6ed32c18029986ce3cadc93bbe1843d274010
yunju <yunju891018@gmail.com>
2022-09-28 11:54:58 +0900

    날짜 선택 (Calendar, DatePickerDialog, simpleDateFormat)

--- app/src/main/java/com/example/keepthetime_project/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/EditAppointmentActivity.kt
@@ -1,12 +1,18 @@
 package com.example.keepthetime_project
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.DatePicker
 import com.example.keepthetime_project.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
 
-    private lateinit var binding : ActivityEditAppointmentBinding
+    private lateinit var binding: ActivityEditAppointmentBinding
+
+    //    약속 시간 저장하는 멤버변수, 현재시간 세팅
+    val mSelectedAppointmentDateTime = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +25,26 @@ class EditAppointmentActivity : BaseActivity() {
     }
 
     override fun setEvents() {
+        binding.txtDate.setOnClickListener {
+            val dateSet = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayofMonth: Int) {
 
+                    mSelectedAppointmentDateTime.set(year, month, dayofMonth)
+                    val simpleDate = SimpleDateFormat("yy/MM/dd")
 
+                    binding.txtDate.text = simpleDate.format(mSelectedAppointmentDateTime.time)
+                }
+            }
+
+            val datePickerDialog = DatePickerDialog(
+                mContext,
+                dateSet,
+                mSelectedAppointmentDateTime.get(Calendar.YEAR),
+                mSelectedAppointmentDateTime.get(Calendar.MONTH),
+                mSelectedAppointmentDateTime.get(Calendar.DAY_OF_MONTH)
+            ).show()
+
+        }
 
     }
 
