398377f60f061a013580ea03205f6e7dfbc571c7
yunju <yunju891018@gmail.com>
2022-10-04 18:35:07 +0900

    네이버맵 경로선 오버레이 테스트

--- app/src/main/java/com/example/keepthetime_project/EditAppointmentActivity.kt
+++ app/src/main/java/com/example/keepthetime_project/EditAppointmentActivity.kt
@@ -15,6 +15,7 @@ import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.PathOverlay
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -23,8 +24,9 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityEditAppointmentBinding
     private val addAppointmentViewModel by viewModels<AddAppointmentViewModel>()
 
-    private var marker : Marker? = null
-    private var selectedLatLng : LatLng? = null
+    private var marker: Marker? = null
+    private var selectedLatLng: LatLng? = null
+    private var path: PathOverlay? = null
 
     //    약속 시간 저장하는 멤버변수, 현재시간 세팅
     val mSelectedAppointmentDateTime = Calendar.getInstance()
@@ -36,7 +38,6 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
 
         setEvents()
         setValues()
-        setMap()
         observer()
     }
 
@@ -63,7 +64,7 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
         }
 
         binding.txtTime.setOnClickListener {
-            val timeSet = object : TimePickerDialog.OnTimeSetListener{
+            val timeSet = object : TimePickerDialog.OnTimeSetListener {
                 override fun onTimeSet(p0: TimePicker?, hourOfDay: Int, minute: Int) {
 
                     mSelectedAppointmentDateTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
@@ -85,29 +86,29 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
         binding.btnSave.setOnClickListener {
 
             val inputTitle = binding.edtTitle.text.toString()
-            if(inputTitle.isEmpty()){
+            if (inputTitle.isEmpty()) {
                 Toast.makeText(mContext, "약속 제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if(binding.txtDate.text == "약속 일자"){
-            Toast.makeText(mContext, "일자를 선택해주세요.", Toast.LENGTH_SHORT).show()
-            return@setOnClickListener
-        }
-            if(binding.txtDate.text == "약속 시간"){
+            if (binding.txtDate.text == "약속 일자") {
+                Toast.makeText(mContext, "일자를 선택해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (binding.txtDate.text == "약속 시간") {
                 Toast.makeText(mContext, "시간을 선택해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             val now = Calendar.getInstance()
-            if(mSelectedAppointmentDateTime.timeInMillis < now.timeInMillis){
+            if (mSelectedAppointmentDateTime.timeInMillis < now.timeInMillis) {
                 Toast.makeText(mContext, "현재 이후의 시간으로 선택해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
             val inputPlaceName = binding.edtPlaceName.text.toString()
-            if(inputPlaceName.isEmpty()){
+            if (inputPlaceName.isEmpty()) {
                 Toast.makeText(mContext, "장소 이름을 입력해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if(selectedLatLng == null){
+            if (selectedLatLng == null) {
                 Toast.makeText(mContext, "약속 장소를 선택해주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -127,16 +128,18 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
     }
 
     override fun setValues() {
+        setMap()
+
 
     }
 
-    private fun observer(){
+    private fun observer() {
 
         addAppointmentViewModel.appointment.observe(this, androidx.lifecycle.Observer {
-            if(it.code == 200){
+            if (it.code == 200) {
                 Toast.makeText(mContext, "약속 등록이 완료되었습니다.", Toast.LENGTH_SHORT).show()
                 finish()
-            }else{
+            } else {
                 Toast.makeText(mContext, it.message, Toast.LENGTH_SHORT).show()
             }
         })
@@ -164,7 +167,7 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
         naverMap.setOnMapClickListener { pointF, latLng ->
 
 //            마커 객체가 없을 시 새 마커 생성
-            if(marker == null){
+            if (marker == null) {
                 marker = Marker()
             }
 
@@ -172,6 +175,16 @@ class EditAppointmentActivity : BaseActivity(), OnMapReadyCallback {
             marker?.map = naverMap
 
             selectedLatLng = latLng
+
+//            coord ~ 선택지까지 직선 그리기 (naverMap PathOverlay)
+//            path 객채가 없을 시 새 객체 생성
+            if (path == null) {
+                path = PathOverlay()
+            }
+            path?.coords = listOf(      //빈 리스트를 만들어서,출발 / 도착지 지정
+                coord, latLng
+            )
+            path?.map = naverMap
 
         }
 
